d447f0b10c49c027b4e38502953155b61688c3f3
Mickey Cowden <mickey@vt.edu>
2016-03-02 20:54:17 -0500

    Initial commit.

---
+++ src/teleop_joystick.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+helpMessage = """
+Use joystick to move UAV.
+---------------------------
+'L': ascend
+'R': descend
+'Right Stick Up': move forward
+'Right Stick Down': move backward
+'Right Stick Left': move left
+'Right Stick Right': move right
+
+CTRL-C to quit
+"""
+
+def joystickCallback(msg):
+  twist = Twist()
+  if (msg.buttons[4]):
+    twist.linear.z = 1.0
+  elif (msg.buttons[5]):
+    twist.linear.z = -1.0
+  twist.linear.x = msg.axes[2]
+  twist.linear.y = -1.0 * msg.axes[3]
+  if (msg.buttons[0]):
+    twist.linear.x = 1000.0
+    twist.linear.y = 1000.0
+    twist.linear.z = 1000.0
+  pub.publish(twist)
+  return 
+
+rospy.init_node('teleop_joystick')
+sub = rospy.Subscriber('joy', Joy, joystickCallback)
+pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+
+print helpMessage
+
+rospy.spin()
+
---
+++ src/teleop_keyboard.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+import sys, select, termios, tty
+
+helpMessage = """
+Use keyboard to move UAV.
+---------------------------
+k: hover
+a: ascend
+d: descend
+l: + x-axis
+j: - x-axis
+i: + y-axis
+,: - y-axis
+w: warp
+
+CTRL-C to quit
+"""
+
+moveBindings = {
+  'k' : (0.0, 0.0, 0.0),
+  'a' : (0.0, 0.0, 1.0),
+  'd' : (0.0, 0.0, -1.0),
+  'l' : (1.0, 0.0, 0.0),
+  'j' : (-1.0, 0.0, 0.0),
+  'i' : (0.0, 1.0, 0.0),
+  ',' : (0.0, -1.0, 0.0),
+  'w' : (100.0, 100.0, 100.0)
+}
+
+def getKey():
+  tty.setraw(sys.stdin.fileno())
+  select.select([sys.stdin], [], [], 0)
+  key = sys.stdin.read(1)
+  termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+  return key
+
+rospy.init_node('teleop_keyboard')
+pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
+settings = termios.tcgetattr(sys.stdin)
+
+print helpMessage
+
+while(1):
+  key = getKey()
+
+  if key in moveBindings.keys():
+    twist = Twist()
+    twist.linear.x = moveBindings[key][0]
+    twist.linear.y = moveBindings[key][1]
+    twist.linear.z = moveBindings[key][2]
+    pub.publish(twist)
+    print twist
+  elif (key == '\x03'):
+    break
+
+  termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
