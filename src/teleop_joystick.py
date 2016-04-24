bed8d853d7eb475fef8a207b15e7bc56a604b508
Mickey Cowden <mickey@vt.edu>
2016-04-24 00:33:49 -0400

    Extracted UAV namespacing to workspace.

--- src/teleop_joystick.py
+++ src/teleop_joystick.py
@@ -32,29 +32,21 @@ def joystickCallback(msg):
   twist.linear.y = msg.axes[2]
   twist.linear.z = msg.axes[5]
   pubMove.publish(twist)
-  if (msg.buttons[8]):
-    pubStop1.publish(Empty())
-    pubStop2.publish(Empty())
+  if (msg.buttons[7]):
+    pubEmergency.publish(Empty())
+  elif (msg.buttons[8]):
+    pubStop.publish(Empty())
   elif (msg.buttons[9]):
-    pubStart1.publish(Empty())
-    pubStart2.publish(Empty())
-  elif (msg.buttons[7]):
-    emergency1.publish(Empty())
-    emergency2.publish(Empty())
-  return 
+    pubStart.publish(Empty())
+  return
 
 rospy.init_node('teleop_joystick')
 sub = rospy.Subscriber('/joy', Joy, joystickCallback)
 pubMove = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
-pubStart1 = rospy.Publisher('/uav1/ardrone/takeoff', Empty, queue_size = 10)
-pubStop1 = rospy.Publisher('/uav1/ardrone/land', Empty, queue_size = 10)
-pubStart2 = rospy.Publisher('/uav2/ardrone/takeoff', Empty, queue_size = 10)
-pubStop2 = rospy.Publisher('/uav2/ardrone/land', Empty, queue_size = 10)
-emergency1 = rospy.Publisher('/uav1/ardrone/reset', Empty, queue_size = 10)
-emergency2 = rospy.Publisher('/uav2/ardrone/reset', Empty, queue_size = 10)
-
+pubStart = rospy.Publisher('/ardrone/takeoff', Empty, queue_size = 10)
+pubStop = rospy.Publisher('/ardrone/land', Empty, queue_size = 10)
+pubEmergency = rospy.Publisher('/ardrone/reset', Empty, queue_size = 10)
 
 print helpMessage
 
 rospy.spin()
-
